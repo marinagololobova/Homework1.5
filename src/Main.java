a44e6f908ef9602282c853bd720b5b8ea118bc27
marinagololobova <marinagololobova@mail.ru>
2022-11-14 20:02:40 +0300

    HW условные операторы 2

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,23 @@ public class Main {
 
         System.out.println("Задание 1");
 
-        int age = 18;
+        int age = 8;
         if(age >= 18){
             System.out.println("Поздравляю тебя с совершеннолетием!");
-        } if(age < 18) {
+        } else {
             System.out.println("Тебе еще нет 18, нужно немного подождать.");
         }
 
         System.out.println("Задание 2");
 
-        int years = 26;
-        if(years >= 7 && years < 18) {
+        int years = 28;
+        if(years < 7) {
+            System.out.println("Ребенок еще совсем маленький");
+        } if(years >= 7 && years < 18) {
             System.out.println("Ребенок ходит в школу");
-        } if(years >= 18 && years < 24){
+        } if(years >= 18 && years < 24) {
             System.out.println("Человек уже закончил школу и может отправляться в университет");
-        } if(years >= 24) {
+        } else {
             System.out.println("Человек окончил университет и ему пора искать первую работу");
         }
 
@@ -25,12 +27,12 @@ public class Main {
         System.out.println("Задание 3");
 
         int trainCarCapacity = 102;
-        int freeSeatsOnTheTrain = 30;
-        if(freeSeatsOnTheTrain >= 60 && freeSeatsOnTheTrain <= 102) {
+        int freeSeatsOnTheTrain = 0;
+        if(freeSeatsOnTheTrain < 103 && freeSeatsOnTheTrain > 59) {
             System.out.println("Есть сидячие и стоячие места");
         } if(freeSeatsOnTheTrain < 60 && freeSeatsOnTheTrain >= 1) {
             System.out.println("Есть только стоячие места");
-        } if(freeSeatsOnTheTrain < 1) {
+        } else if(freeSeatsOnTheTrain < 1) {
             System.out.println("Все места заняты");
         }
 
